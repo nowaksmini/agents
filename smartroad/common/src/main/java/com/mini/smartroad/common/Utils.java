bf33658d2eb4dda54c5929c99d979d20ff36396e
Sylwia Nowak <Sylwia.Nowak@coi.gov.pl>
2016-08-31 15:45:00 +0200

    * module structure

--- smartroad/common/src/main/java/com/mini/smartroad/common/Utils.java
+++ smartroad/common/src/main/java/com/mini/smartroad/common/Utils.java
@@ -1,7 +1,5 @@
 package com.mini.smartroad.common;
 
-import jade.core.AID;
-
 public class Utils {
     public static final int USER_SERVICE_ID = 1;
     public static final int USER_SIMULATOR_ID = 2;
@@ -15,13 +13,4 @@ public class Utils {
 
     public static final String IP = "192.168.0.38";
     public static final String PORT = "1099";
-    //to tylko na glownym
-    public static final AID[] spammers = {
-            new AID("sa0", AID.ISLOCALNAME),
-            new AID("sa1", AID.ISLOCALNAME),
-    };
-    public static final AID[] consumers = {
-            new AID("mca0", AID.ISLOCALNAME),
-            new AID("mca1", AID.ISLOCALNAME)
-    };
 }
