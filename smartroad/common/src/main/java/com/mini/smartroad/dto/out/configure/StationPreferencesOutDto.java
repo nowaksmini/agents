9e7d3b73fc5e68ff7cc72219a1727c6315348e02
Sylwia Nowak <sylwia.nowak@connectis.pl>
2017-09-09 02:43:33 +0200

    Refactor code.

--- smartroad/common/src/main/java/com/mini/smartroad/dto/out/configure/StationPreferencesOutDto.java
+++ smartroad/common/src/main/java/com/mini/smartroad/dto/out/configure/StationPreferencesOutDto.java
@@ -1,17 +1,14 @@
 package com.mini.smartroad.dto.out.configure;
 
-import com.mini.smartroad.dto.in.BaseInDto;
 import com.mini.smartroad.dto.out.StatusOutDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
-public class StationPreferencesOutDto extends BaseInDto implements Serializable {
+public class StationPreferencesOutDto implements Serializable {
     private String name;
     private String email;
     private String phone;
